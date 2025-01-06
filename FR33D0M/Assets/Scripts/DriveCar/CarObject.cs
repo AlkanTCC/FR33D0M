79a8e1c06b042d44be99d98369921c40fbd56fc1
Alkan Cakir <0355617@student.rocvantwente.nl>
2025-01-06 15:06:45 +0100

    Feat: Car can stop

--- FR33D0M/Assets/Scripts/DriveCar/CarObject.cs
+++ FR33D0M/Assets/Scripts/DriveCar/CarObject.cs
@@ -5,15 +5,27 @@ public class CarObject : ScriptableObject
 {
     // Gemaakt door: Alkan Cakir
 
+    [Header("Information")]
     public new string name;
 
+    [Header("Speed")]
     public float maxSpeed;
 
+    [Header("Acceleration")]
     public float initialAcceleration;
     public float accelerationRate;
+    public float accelerationStartTime;
 
+    [Header("Deceleration")]
     public float decelerationRate;
 
+    [Header("Break")]
+    public float breakRate;
+
+    [Header("Rotation")]
+    public float rotationRate;
+
+    [Header("Car")]
     public bool openSeat;
     public bool playerInCar;
 }
--- FR33D0M/Assets/Scripts/DriveCar/DriveCarSystem.cs
+++ FR33D0M/Assets/Scripts/DriveCar/DriveCarSystem.cs
@@ -11,6 +11,10 @@ public class DriveCarSystem : MonoBehaviour
 
     float speed;
 
+    float accelerationTime;
+
+    bool isBreaking = false;
+
     private void Start()
     {
         // Zoekt de CarManager script in de scene en zet die in de carManager
@@ -20,20 +24,28 @@ public class DriveCarSystem : MonoBehaviour
 
         // Gaat in de carManager en daarna in de carObjectsArray en pakt een random CarObject die in de array zit en zet die dan in de carObject
         carObject = carManager.carObjectsArray[Random.Range(0, carManager.carObjectsArray.Length)];
+
+        accelerationTime = carObject.accelerationStartTime;
     }
 
     private void Update()
     {
         DriveSystem();
-
-        print(speed);
     }
 
     void DriveSystem()
     {
+        // Als de player niet in de auto zit gebeurd de code hier onder niet
         if (!CheckPlayerInCar()) return;
 
-        rb.linearVelocity = new Vector2(0, speed);
+        // Beweeg de auto omhoog keer de speed
+        rb.linearVelocity = transform.up * speed;
+
+        if (speed != 0)
+        {
+            ChangeDirection();
+            CheckBreakCar();
+        }
 
         if (!CheckCarForward())
         {
@@ -42,31 +54,58 @@ public class DriveCarSystem : MonoBehaviour
         }
 
         AccelerateCar();
-        ChangeDirection();
     }
 
     void AccelerateCar()
     {
-        speed += carObject.initialAcceleration * Mathf.Pow(Time.time, 2) * carObject.accelerationRate * Time.deltaTime;
+        // Dit zorgt ervoor dat de speed value steeds sneller omhoog gaat
+        speed += carObject.initialAcceleration * Mathf.Pow(accelerationTime, 2) * carObject.accelerationRate * Time.deltaTime;
 
+        // Dit zorgt ervoor dat de kleinste nummer van de twee gereturned word. Dat zorgt ervoor dat de speed niet boven de max speed komt
         speed = Mathf.Min(speed, carObject.maxSpeed);
     }
 
     void DecelerateCar()
     {
-        speed -= carObject.initialAcceleration * Mathf.Pow(Time.time, 2) * carObject.decelerationRate * Time.deltaTime;
-        speed = Mathf.Max(speed, 0);
+        speed -= carObject.initialAcceleration * Mathf.Pow(accelerationTime, 2) * carObject.decelerationRate * Time.deltaTime;
+        
+        if (!isBreaking) speed = Mathf.Max(speed, 0);
+
+        if (speed == 0) accelerationTime = carObject.accelerationStartTime;
     }
 
     void ChangeDirection()
     {
-        if (Input.GetKey(KeyCode.A)) transform.Rotate(new Vector3(0, 0, 0.3f));
-        else if (Input.GetKey(KeyCode.D)) transform.Rotate(new Vector3(0, 0, -0.3f));
+        // Rotate de auto naar links of naar rechts (licht eraan welke knop je indrukt)
+        if (Input.GetKey(KeyCode.A)) transform.Rotate(new Vector3(0, 0, carObject.rotationRate * speed));
+        else if (Input.GetKey(KeyCode.D)) transform.Rotate(new Vector3(0, 0, -carObject.rotationRate * speed));
+    }
+
+    void CheckBreakCar()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            isBreaking = true;
+            BreakCar();
+        }
+        else if (Input.GetKeyUp(KeyCode.S)) isBreaking = false;
+    }
+
+    void BreakCar()
+    {
+        speed = speed - carObject.breakRate * Time.deltaTime;
+        speed = Mathf.Max(speed, 0);
     }
 
     bool CheckCarForward()
     {
-        if (Input.GetKey(KeyCode.W)) return true;
+        if (Input.GetKey(KeyCode.W))
+        {
+            accelerationTime += Time.deltaTime;
+            accelerationTime = Mathf.Min(accelerationTime, 2);
+            return true;
+        }
+
         return false;
     }
 
