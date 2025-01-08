8c530f498d0ba222b7cf99958e9695e4832381c0
Abdurrahman Esat Yavuz <abdesatyavuz@gmail.com>
2025-01-08 15:37:44 +0100

    Updated PlayerRotation

--- FR33D0M/Assets/Scripts/PlayerRotation/PlayerRotation.cs
+++ FR33D0M/Assets/Scripts/PlayerRotation/PlayerRotation.cs
@@ -4,30 +4,17 @@ public class PlayerRotation : MonoBehaviour
 {
     // Script gemaakt door Esat Yavuz
 
-    bool mouseToMovement = false;
-
-    public float distanceToMouse = 4f;
     public float playerRotationSpeed = 5f;
 
-    Vector2 mousePos;
+    public Vector2 mousePos;
 
-    Quaternion playerMoveRotation;
-    Quaternion playerMouseRotation;
+    public Quaternion playerMouseRotation;
 
     // Update is called once per frame
     void Update()
     {
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        if (Vector2.Distance(transform.position, mousePos) < distanceToMouse)
-        {
-            RotatePlayerToMovement();
-            return;
-        }
-        else
-        {
-            mouseToMovement = true;
-            RotatePlayerToMousePos();
-        }
+        RotatePlayerToMousePos();
     }
 
     void RotatePlayerToMousePos()
@@ -35,21 +22,5 @@ public class PlayerRotation : MonoBehaviour
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         playerMouseRotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, playerMouseRotation, playerRotationSpeed * Time.deltaTime);
-    }
-
-    void RotatePlayerToMovement()
-    {
-        if (mouseToMovement)
-        {
-            playerMoveRotation = playerMouseRotation;
-            mouseToMovement = false;
-        }
-
-        if (Input.GetKey(KeyCode.W)) playerMoveRotation = Quaternion.AngleAxis(0, Vector3.forward);
-        if (Input.GetKey(KeyCode.A)) playerMoveRotation = Quaternion.AngleAxis(-270, Vector3.forward);
-        if (Input.GetKey(KeyCode.S)) playerMoveRotation = Quaternion.AngleAxis(-180, Vector3.forward);
-        if (Input.GetKey(KeyCode.D)) playerMoveRotation = Quaternion.AngleAxis(-90, Vector3.forward);
-
-        transform.rotation = Quaternion.Slerp(transform.rotation, playerMoveRotation, playerRotationSpeed * Time.deltaTime);
     }
 }
