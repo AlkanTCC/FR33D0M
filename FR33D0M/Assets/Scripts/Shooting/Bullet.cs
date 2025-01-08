cfeac85529bde73e24e72056dce129e9568b7082
Jin-Aljumaili <adamaljumailiprivate@gmail.com>
2025-01-08 11:00:37 +0100

    Check b4 push

--- FR33D0M/Assets/Scripts/Shooting/Bullet.cs
+++ FR33D0M/Assets/Scripts/Shooting/Bullet.cs
@@ -6,13 +6,10 @@ public class Bullet : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     PlayerShoot playerShoot;
     Vector2 position;
-    Vector2 posAtShooting;
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerShoot = FindFirstObjectByType<PlayerShoot>();
-        posAtShooting = transform.position;
         
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,7 +25,6 @@ public class Bullet : MonoBehaviour
     {
         position = transform.position;
         rb.linearVelocity = new Vector2(0, 10);
-        print($"position at shooting >{posAtShooting}, position of bullet > {position}");
         if (Vector2.Distance(playerShoot.firePoint.position, position) > playerShoot.currentWeapon.range)
         {
             print("out of range");
