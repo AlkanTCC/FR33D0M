5eb09df2960de2f0307c880fd54c7f6e13fd7764
Jin-Aljumaili <adamaljumailiprivate@gmail.com>
2025-01-08 14:19:04 +0100

    Created a skeleton for damage animation, before merging to it's own feature

---
+++ FR33D0M/Assets/Scripts/DamageNumbers/DamagePopUp.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
+public class DamagePopUp : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
