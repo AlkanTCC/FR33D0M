f37c9e0399f29156dfb103efe995f18ff3d8aa63
Jin-Aljumaili <adamaljumailiprivate@gmail.com>
2025-01-09 10:52:38 +0100

    DamageNumbers added and fixed
    Damage numbers work as intended now

--- FR33D0M/Assets/Scripts/Enemies/TakeDamage.cs
+++ FR33D0M/Assets/Scripts/Enemies/TakeDamage.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class TakeDamage : MonoBehaviour
 {
-    public int HP;
+    public int HP = 500;
     bool agro = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- FR33D0M/Assets/Scripts/Shooting/Bullet.cs
+++ FR33D0M/Assets/Scripts/Shooting/Bullet.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 // gemaakt door Jin aljumaili
 public class Bullet : MonoBehaviour
 {
+
+    public GameObject damagePreFab1,damagePreFab2, damagePreFab3, enemy;
+    TextMeshPro textMesh;
     [SerializeField] Rigidbody2D rb;
     TakeDamage takeDamage;
     PlayerShoot playerShoot;
@@ -13,6 +17,8 @@ public class Bullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        
+        enemy = FindFirstObjectByType<TakeDamage>().gameObject;
         takeDamage = FindFirstObjectByType<TakeDamage>();
         playerShoot = FindFirstObjectByType<PlayerShoot>();
         player = FindAnyObjectByType<PlayerMovement>().gameObject;
@@ -21,9 +27,27 @@ public class Bullet : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            takeDamage.HP = -playerShoot.currentWeapon.damage;
+            takeDamage.HP -= playerShoot.currentWeapon.damage;
+            GameObject damageInstance = Instantiate(GetPopUp(), enemy.transform);
+            damageInstance.transform.GetChild(0).gameObject.GetComponent<TextMeshPro>().text = playerShoot.currentWeapon.damage.ToString();
             print(takeDamage.HP);
             gameObject.SetActive(false);
+        }
+    }
+    private GameObject GetPopUp()
+    {
+        int random = Random.Range(0,3);
+        if (random == 0)
+        {
+            return damagePreFab1;
+        }
+        else if (random == 1)
+        {
+            return damagePreFab2;
+        }
+        else
+        {
+            return damagePreFab3;
         }
     }
    
