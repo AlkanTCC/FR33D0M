0eb729a79366ae158cd316a0c95ba3927635adc0
Alkan Cakir <0355617@student.rocvantwente.nl>
2025-01-13 15:17:21 +0100

    Feat: Enemy death

--- FR33D0M/Assets/Scripts/Alkan/DriveCar/DriveCarSystem.cs
+++ FR33D0M/Assets/Scripts/Alkan/DriveCar/DriveCarSystem.cs
@@ -91,6 +91,6 @@ public class DriveCarSystem : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<NPCWalkingSystem>() != null) collision.gameObject.GetComponent<NPCWalkingSystem>().CarCollision(rb);
+        if (collision.gameObject.GetComponent<NPCWalkingSystem>() != null) collision.gameObject.GetComponent<NPCWalkingSystem>().CarCollision(this);
     }
 }
