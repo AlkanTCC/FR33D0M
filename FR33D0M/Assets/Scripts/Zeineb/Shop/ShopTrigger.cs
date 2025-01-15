0b4a5e10d70f8477b8ea387fd9f6ff0e8e481fe8
zeinebibrahim <zeineb.ibrahim01@outlook.com>
2025-01-15 16:33:02 +0100

    fix:shop

--- FR33D0M/Assets/Scripts/Zeineb/Shop/ShopTrigger.cs
+++ FR33D0M/Assets/Scripts/Zeineb/Shop/ShopTrigger.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class ShopTrigger : MonoBehaviour
 {
+    public Canvas shopCanvas;
     //Gemaakt door: Zeineb
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMovement>() != null)
         {
-            Debug.Log("gujzzvj");
+            print("bye");
+            shopCanvas.gameObject.SetActive(true);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        /*if (collision.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            print("hi");
+            shopCanvas.gameObject.SetActive(false);
+        }*/
     }
 }
