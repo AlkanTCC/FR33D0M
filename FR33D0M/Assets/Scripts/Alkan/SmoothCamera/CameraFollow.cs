6ff50f4606abdfe94a8430b94623cf39b1c52195
Abdurrahman Esat Yavuz <abdesatyavuz@gmail.com>
2025-01-15 15:35:22 +0100

    feat: camera zoom and movement added

--- FR33D0M/Assets/Scripts/Alkan/SmoothCamera/CameraFollow.cs
+++ FR33D0M/Assets/Scripts/Alkan/SmoothCamera/CameraFollow.cs
@@ -5,35 +5,37 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Vector3 offset;
     [SerializeField] float speed = 3f;
-    [SerializeField] float offsetMultitude = 3f;
+    [SerializeField] float offsetMultitude = 2.25f;
 
     GameObject player;
     DriveCarSystem car;
+    Camera cam;
 
 
     private void Start()
     {
         player = FindAnyObjectByType<PlayerMovement>().gameObject;
         car = FindAnyObjectByType<DriveCarSystem>();
+        cam = GetComponent<Camera>();
     }
 
     private void LateUpdate()
     {
+        cam.orthographicSize = Mathf.Clamp(1.00001f * car.currentSpeed, 6, 8);
         if (car.carObject.playerInCar) PlayerInCarCamera();
         else PlayerCamera();
-        transform.position = Vector3.Slerp(transform.position, new Vector3((player.transform.position + offset).x, (player.transform.position + offset).y, -10), speed * Time.deltaTime);
     }
 
     void PlayerCamera()
     {
-
         offset.x = Input.GetAxisRaw("Horizontal") * offsetMultitude;
         offset.y = Input.GetAxisRaw("Vertical") * offsetMultitude;
+        transform.position = Vector3.Slerp(transform.position, new Vector3((player.transform.position + offset).x, (player.transform.position + offset).y, -10), speed * Time.deltaTime);
     }
 
     void PlayerInCarCamera()
     {
-        offset.x = car.transform.rotation.z * offsetMultitude;
-        offset.y = car.transform.rotation.z * offsetMultitude;
+        if (car.currentSpeed >= -1.1f) transform.position = Vector3.Slerp(transform.position, new Vector3(car.transform.GetChild(0).transform.position.x, car.transform.GetChild(0).transform.position.y, -10), speed * Time.deltaTime);
+        else transform.position = Vector3.Slerp(transform.position, new Vector3(car.transform.GetChild(1).transform.position.x, car.transform.GetChild(1).transform.position.y, -10), speed * Time.deltaTime);
     }
 }
